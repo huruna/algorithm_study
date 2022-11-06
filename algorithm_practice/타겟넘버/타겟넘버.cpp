8ec7da23511ba9286761d20db1bc4b4b7314dd2b
kje01 <91876894+huruna@users.noreply.github.com>
2022-11-06 23:31:51 +0900

    Update 타겟넘버.cpp

--- algorithm_practice/타겟넘버/타겟넘버.cpp
+++ algorithm_practice/타겟넘버/타겟넘버.cpp
@@ -1,1 +1,24 @@
+#include <string>
+#include <vector>
 
+using namespace std;
+
+int answer = 0;
+int tg = 0;
+
+void dfs (vector<int> numbers, int total, int n){
+    if(n == numbers.size()){
+        if(total == tg){
+            answer++;
+        }
+        return;
+    }
+    dfs(numbers, total + numbers[n], n + 1);
+    dfs(numbers, total - numbers[n], n + 1);
+}
+
+int solution(vector<int> numbers, int target) {
+    tg = target;
+    dfs(numbers, 0, 0);
+    return answer;
+}
