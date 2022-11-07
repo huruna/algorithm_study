a899c7ccd44aeda1bc49797cdb993febf40375fd
kje01 <91876894+huruna@users.noreply.github.com>
2022-11-07 16:08:18 +0900

    Update 큰수만들기.cpp

--- algorithm_practice/큰수만들기/큰수만들기.cpp
+++ algorithm_practice/큰수만들기/큰수만들기.cpp
@@ -1,3 +1,22 @@
 #include <string>
 #include <vector>
-#include <iostream>
+
+using namespace std;
+
+string solution(string number, int k) 
+{
+    int count = 0;
+    for(int i = 1; i <= k; i++) {      
+        for(int j = 0; j < number.size() - 1; j++) {
+           if(number[j] < number[j + 1]) {
+                number.erase(j, 1);
+                count++;
+                break;
+           }
+        }   
+    }
+    if(count != k) {
+        number.erase(number.size() - (k - count), k - count);
+    }
+    return number;
+}
