39097cb7836a22897c375547cc816f5e16dc5b01
kje01 <91876894+huruna@users.noreply.github.com>
2022-11-10 00:57:34 +0900

    Create 소믈리에구름이.cpp

---
+++ 2022 구름모의테스트/소믈리에구름이/소믈리에구름이.cpp
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <malloc.h>
+int main() {
+	int n;
+	int answer = 0;
+	int* cup = (int*)malloc(sizeof(int) * 2000000);
+	scanf("%d", &n);
+	for (int i = 1; i <= n; i++) {
+		scanf("%d ", &cup[i - 1]);
+	}
+	int j;
+	int cnt = -1;
+	int max = cup[0];
+	int max_idx = 0;
+	while (cnt != n - 1) {
+		for (j = 0; j < n; j++) {
+			if (max < cup[j]) {
+				max = cup[j];
+				max_idx = j;
+			}
+		}
+		for (j = 0; j < n; j++) {
+			if (j != max_idx)
+				cup[j] += 1;
+		}
+		answer++;
+		cnt = 0;
+		for (j = 0; j < n - 1; j++) {
+			if (cup[j] == cup[j + 1])
+				cnt++;
+		}
+	}
+	free(cup);
+	printf("%d", answer);
+	return answer;
+}
